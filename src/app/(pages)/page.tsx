98c3cfb60fb87b48f014a223b5e5f60533e11596
Szymon Wojciechowski <kierej@gmail.com>
2023-10-11 20:24:20 +0200

    nice

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
     <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">Hello</div>
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        Hello
+        <Button>Button</Button>
+      </div>
     </section>
   );
 }
