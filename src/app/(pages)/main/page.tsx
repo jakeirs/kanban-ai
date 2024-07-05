eaf617620f137515827782920df6b8718507ba74
Simon <kierej@gmail.com>
2024-07-05 18:40:16 +0200

    added page transition

--- src/app/(pages)/main/page.tsx
+++ src/app/(pages)/main/page.tsx
@@ -1,25 +1,14 @@
+"use client";
+
 import ThemeToggle from "@/components/blocks/theme-toggle";
 import { Button } from "@/components/ui/button";
 
-export default async function MainPage() {
-  const userStatus = localStorage.getItem("userStatus");
-
-  if (userStatus !== "1") {
-    return (
-      <div>
-        <div>User not authorized. Didn't pay</div>
-        <div>You can pay here...</div>
-      </div>
-    );
-  }
-
-  if (userStatus === "1") {
-    return (
-      <div>
-        <ThemeToggle />
-        It's a paid course accesses
-        <Button variant="destructive" asChild></Button>
-      </div>
-    );
-  }
+export default function MainPage() {
+  return (
+    <div>
+      <ThemeToggle />
+      It's a paid course accesses
+      <Button variant="destructive" asChild></Button>
+    </div>
+  );
 }
