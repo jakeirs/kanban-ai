67bc0c9f8e88daad41165edba2c8d7e4e77f5e2c
Simon <kierej@gmail.com>
2024-12-28 17:57:53 +0700

    asd

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,3 +1,4 @@
+import { kanbanBoardsTable } from "./tables/kanban/table";
 import GitHub from "@auth/core/providers/github";
 import { convexAuth } from "@convex-dev/auth/server";
 import { MutationCtx } from "./_generated/server";
@@ -7,9 +8,19 @@ export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [GitHub],
   callbacks: {
     /**
-     * Prepare App For New User
+     * This functions calls after any login | or sign
      */
     async afterUserCreatedOrUpdated(ctx: MutationCtx, { userId }) {
+      const hasUserKanbanBoard = await ctx.db
+        .query("kanbanBoards")
+        .filter((q) => q.eq(q.field("ownerUserId"), userId))
+        .first();
+
+      if (hasUserKanbanBoard) {
+        return;
+      }
+
+      // Init KanbanBoard For New User
       const kanbanBoardId = await ctx.runMutation(
         internal.tables.kanban.logic.initKanbanBoardLogic.default,
         {
