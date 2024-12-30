bee0c7f6490d52fe68d98e43062f380a782a0c39
Simon <kierej@gmail.com>
2024-12-30 21:42:22 +0700

    middleware catch try

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,13 +8,18 @@ import {
 const isSignInPage = createRouteMatcher(["/"]);
 const isProtectedRoute = createRouteMatcher(["/kanban(.*)"]);
 
-export default convexAuthNextjsMiddleware(
-  async (request, { convexAuth }) => {
+export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+  try {
     if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
       return nextjsMiddlewareRedirect(request, "/");
     }
-  },
-);
+  } catch (error: any) {
+    console.error("Middleware error:", error);
+    console.log("Middleware error:", error);
+
+    return nextjsMiddlewareRedirect(request, "/error");
+  }
+});
 
 export const config = {
   // The following matcher runs middleware on all routes
