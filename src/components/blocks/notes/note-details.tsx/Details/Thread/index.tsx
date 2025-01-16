74e1571f8f14fd922726bfd328895fff105e49d7
Simon <kierej@gmail.com>
2025-01-16 16:24:12 +0700

    asd

--- src/components/blocks/notes/note-details.tsx/Details/Thread/index.tsx
+++ src/components/blocks/notes/note-details.tsx/Details/Thread/index.tsx
@@ -5,27 +5,27 @@ import { MessageCircle } from "lucide-react";
 
 interface ThreadProps {
   color?: string;
-  iconColor?: string;
   className?: string;
 }
 
-export const Thread = ({
-  color = "#FFB5E8",
-  iconColor = "#000000",
-  className,
-}: ThreadProps) => {
+export const Thread = ({ color = "#FFB5E8", className }: ThreadProps) => {
   return (
-    <div className={cn("w-full rounded-full p-1 bg-secondary/20", className)}>
+    <div className={cn("w-full rounded-3xl p-1 bg-secondary/20", className)}>
       <div
-        className="flex w-full items-center gap-4 rounded-full px-6 py-3"
+        className="flex w-full items-center gap-4 rounded-2xl px-10 py-1"
         style={{
           backgroundColor: color,
         }}
       >
         <div className="bg-white rounded-full p-3 flex justify-center">
-          <MessageCircle className="w-full h-full" color={iconColor} />
+          <MessageCircle className="w-full h-full" />
         </div>
-        <span className="text-3xl font-semibold">Details</span>
+        <div className="flex flex-col">
+          <div className="text-xs text-gray-500 leading-[1]">Thread</div>
+          <div className="text-3xl text-white font-semibold leading-[1]">
+            Details
+          </div>
+        </div>
       </div>
     </div>
   );
